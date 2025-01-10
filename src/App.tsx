31452dc12e5b0fcac985933703c0e63ab7cd6311
Mohamad-Aljaber <mohamadaljaber452@gmail.com>
2025-01-11 00:19:51 +0300

    c

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
 import BackToTop from "./components/GoToWhatsApp";
-import i18n from "i18next"; 
 import "./i18n";  
 
 const App = () => {
-  const { t } = useTranslation();
 
 
   return (
     <>
       <BackToTop />
       <Outlet />
-      <h2>{t("welcome")}</h2> 
-      <button
-        onClick={() => {
-          i18n.changeLanguage("ar"); 
-        }}
-      >
-        Arabic
-      </button>
-      <button
-        onClick={() => {
-          i18n.changeLanguage("en"); 
-        }}
-      >
-        English
-      </button>
     </>
   );
 };
